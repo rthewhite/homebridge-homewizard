4d9f1869e9f36cd201a0d57d3f3088a72b8161b2
Raymond de Wit <raymond@thewhite.nl>
2016-03-13 09:25:16 +0100

    Fixed contact sensors

--- src/accessories/contact-sensor.js
+++ src/accessories/contact-sensor.js
@@ -17,13 +17,13 @@ export class HomeWizardContactSensor extends HomeWizardBaseAccessory {
 
   getContactSensorState(callback) {
     this.api.getStatus(this.id, 'kakusensors').then(sensor => {
-      const contact = sensor.status === null
+      const contact = sensor.status === 'yes'
         ? this.hap.Characteristic.ContactSensorState.CONTACT_NOT_DETECTED
         : this.hap.Characteristic.ContactSensorState.CONTACT_DETECTED;
 
       switch (contact) {
         case this.hap.Characteristic.ContactSensorState.CONTACT_NOT_DETECTED:
-          this.log(`Non detected contact at sensor:${this.name}`);
+          this.log(`No contact detected at sensor:${this.name}`);
           break;
         case this.hap.Characteristic.ContactSensorState.CONTACT_DETECTED:
           this.log(`Detected contact at sensor:${this.name}`);
